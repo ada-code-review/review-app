da8f42c062fb110434d8801164c63a3f79ff40ad
Shannon Capper <shannon@textio.com>
2019-08-16 11:02:08 -0700

    Add real contents to UnauthorizedPage

--- src/UnauthorizedPage.tsx
+++ src/UnauthorizedPage.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
-import { Main } from './sharedStyleComponents';
+import { Header1, BodyText, BodyTextLink } from './sharedStyleComponents';
+import { PageWithFooter } from './PageWithFooter';
 
-// TODO: fill out this page with real markup
+// TODO: make this link do something
 export const UnauthorizedPage = () => (
-    <Main>
-        You are not authorized to access this app.
-    </Main>
-);
+    <PageWithFooter>
+          <Header1>Whoops...</Header1>
+          <BodyText>
+            It looks like you aren’t on our current roster of volunteers.
+            If that’s incorrect or you have other questions, please reach
+            out and <BodyTextLink>contact an administrator</BodyTextLink>. 
+          </BodyText>
+    </PageWithFooter>
+  );
